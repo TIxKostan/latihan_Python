2e3954a07650eb84b4965ad63f72706ffe11dac8
TI Kostan <133191630+TIxKostan@users.noreply.github.com>
2023-05-12 11:37:15 +0700

    Update positif_negatif.py
    Update jadi ada 2 versi, yang pertama if else biasa dan yang kedua if else ternary operator

--- positif_negatif.py
+++ positif_negatif.py
@@ -1,3 +1,5 @@
+# if else normal version
+'''
 def positif_negatif(n):
     if n > 0:
         print(f'{n} adalah bilangan positif')
@@ -5,7 +7,20 @@ def positif_negatif(n):
         print(f'{n} adalah bilangan negatif')
     else:
         print(n)
-    
+'''
+
+# ternary operation version
+def positif_negatif(n):
+    print(f'{n} adalah bilangan positif') if n > 0 else print(f'{n} adalah bilangan negatif') if n < 0 else print(f'{0}')
+   
 Bilangan = int(input('Masukkan sebuah bilangan: '))
 
 print(positif_negatif(Bilangan))
+
+'''
+Penjelasan
+Ternary operator (value if condition else value)
+
+jadi python akan print(n adalah positif) if n > 0 else python akan print(n adalah negatif) if n < 0 else print(n adalah 0)
+dibaca pelan-pelan aja gess
+'''
