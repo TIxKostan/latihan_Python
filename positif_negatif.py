f588951f9c6f20ae4e9eb09befe6152f7b591396
TI Kostan <133191630+TIxKostan@users.noreply.github.com>
2023-05-12 10:55:03 +0700

    positif_negatif.py
    Ada sedikit typo, tapi sudah diperbaiki.

--- positif_negatif.py
+++ positif_negatif.py
@@ -1,4 +1,4 @@
-def posifit_negatif(n):
+def positif_negatif(n):
     if n > 0:
         print(f'{n} adalah bilangan positif')
     elif n < 0:
@@ -8,4 +8,4 @@ def posifit_negatif(n):
     
 Bilangan = int(input('Masukkan sebuah bilangan: '))
 
-print(posifit_negatif(Bilangan))
+print(positif_negatif(Bilangan))
